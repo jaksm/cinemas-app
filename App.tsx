45d9c3dc23b2763f2101af514f462b84b7e13ba0
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-02-17 14:25:24 +0100

    code cleanup

--- App.tsx
+++ App.tsx
@@ -1,17 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {SafeAreaView, StatusBar, Text} from 'react-native';
-
-declare var global: {HermesInternal: null | {}};
 
 const App = () => {
   return (
