9b5a2e28adcce33daeb9220c5ffe1b180bb98578
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-02-17 14:58:19 +0100

    added navigation deps

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>Hello Cinemas!</Text>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
