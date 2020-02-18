914a6a4bb042d40e84905fe9134e153863eaf8ce
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-02-18 16:07:03 +0100

    updated safe screen component

--- src/components/SafeScreen.tsx
+++ src/components/SafeScreen.tsx
@@ -1,8 +1,21 @@
 import React, {FC} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, ViewStyle} from 'react-native';
 
-const SafeScreen: FC = ({children}) => {
-  return <SafeAreaView>{children}</SafeAreaView>;
+interface SafeScreenProps {
+  style?: ViewStyle;
+}
+
+const SafeScreen: FC<SafeScreenProps> = ({children, style}) => {
+  return (
+    <SafeAreaView style={[styles.container, style]}>{children}</SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  } as ViewStyle,
+});
 
 export default SafeScreen;
