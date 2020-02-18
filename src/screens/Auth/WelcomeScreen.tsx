283a3e46eb5911a811790c2c607e2e5dbcbc02aa
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-02-18 16:24:16 +0100

    finished welcome screen

--- src/screens/Auth/WelcomeScreen.tsx
+++ src/screens/Auth/WelcomeScreen.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import {Image, StyleSheet, Text, ViewStyle, View} from 'react-native';
-import SafeScreen from '../../components/SafeScreen';
+
+import {
+  Image,
+  StyleSheet,
+  Text,
+  ViewStyle,
+  View,
+  TextStyle,
+  TouchableOpacity,
+} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native';
+
 import theme from '../../theme';
+import SafeScreen from '../../components/SafeScreen';
 import Button from '../../components/Button';
 
 const WelcomeScreen = () => {
+  const navigation = useNavigation();
+
+  function handleSignUpPress() {
+    navigation.navigate('SignUp');
+  }
+
+  function handleSignInPress() {
+    navigation.navigate('SignIn');
+  }
+
   return (
     <SafeScreen style={styles.container}>
       <View style={styles.content}>
@@ -13,7 +35,18 @@ const WelcomeScreen = () => {
           <Text style={theme.typography.h1}>Take Your Seat</Text>
         </View>
 
-        <Button fullWidth={true}>Continue</Button>
+        <View>
+          <Button fullWidth={true} onPress={handleSignUpPress}>
+            Sign up
+          </Button>
+
+          <TouchableOpacity activeOpacity={0.9} onPress={handleSignInPress}>
+            <Text style={[theme.typography.p, styles.text]}>
+              Already have account?{' '}
+              <Text style={styles.signInText}>Sign in</Text> instead.
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeScreen>
   );
@@ -26,7 +59,7 @@ const styles = StyleSheet.create({
   } as ViewStyle,
   content: {
     flex: 1,
-    maxHeight: '60%',
+    maxHeight: '70%',
     justifyContent: 'space-between',
     paddingHorizontal: theme.screenPadding,
   } as ViewStyle,
@@ -34,6 +67,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   } as ViewStyle,
+  text: {
+    alignSelf: 'center',
+    marginTop: 14 * 1.5,
+  } as TextStyle,
+  signInText: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  } as TextStyle,
 });
 
 export default WelcomeScreen;
