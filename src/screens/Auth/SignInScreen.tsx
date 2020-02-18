d14715d8f4b09fa59456d757e5abd74fa63e5dcb
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-02-18 23:25:50 +0100

    added signin screen

--- src/screens/Auth/SignInScreen.tsx
+++ src/screens/Auth/SignInScreen.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {KeyboardAvoidingView, StyleSheet, Text, View, ViewStyle} from 'react-native';
+import Screen from "../../components/Screen";
+import {useNavigation} from "@react-navigation/native";
+import TextInput from "../../components/TextInput";
+import Button from "../../components/Button";
+import theme from "../../theme";
 
 const SignInScreen = () => {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    // navigation.navigate('Home');
+  }
+
   return (
-    <View>
-      <Text>SignInScreen</Text>
-    </View>
+    <Screen>
+      <Text style={theme.typography.h1}>Sign in</Text>
+
+      <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.inputs}>
+          <TextInput
+            placeholder={'john@doe.com'}
+            keyboardType={'email-address'}
+            autoCompleteType={'email'}
+            autoCorrect={false}
+            autoCapitalize={'none'}
+          />
+          <TextInput
+            placeholder={'*******'}
+            autoCorrect={false}
+            autoCapitalize={'none'}
+            secureTextEntry={true}
+          />
+        </View>
+
+        <Button onPress={handleSignIn}>Sign In</Button>
+      </KeyboardAvoidingView>
+    </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    flex: 1,
+    maxHeight: '80%',
+  } as ViewStyle,
+  inputs: {
+    marginTop: 14 * 2,
+  } as ViewStyle,
+});
 
 export default SignInScreen;
--- src/screens/Auth/SignUpScreen.tsx
+++ src/screens/Auth/SignUpScreen.tsx
@@ -14,6 +14,7 @@ import {useNavigation} from '@react-navigation/native';
 
 const SignUpScreen = () => {
   const navigation = useNavigation();
+
   function handleSignUp() {
     // navigation.navigate('Home');
   }
